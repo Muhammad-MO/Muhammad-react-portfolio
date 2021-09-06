017e717982daa201e8f33be513b0cea8834864b1
Muhammad-MO <64999609+Muhammad-MO@users.noreply.github.com>
2021-09-06 20:50:37 +0800

    added image and header on intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -10,8 +10,19 @@ export default function Intro() {
              </div>
               </div>  
 
-         <div className="right"></div>
-        
+         <div className="right">
+          <div className="wrapper">
+
+              <h2> Hi There, I'm</h2>
+              <h1>Muhammad bin Mahmood</h1>
+              <h3>Freelance <span></span> Web Developer</h3>
+
+          </div>
+      <a href="#portfolio">
+          <img src="assets/down.png" alt=""/>
+      </a>
+
+     </div> 
         </div>
     )
 }
